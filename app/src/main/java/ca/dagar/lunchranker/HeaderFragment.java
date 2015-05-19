46bc0047efccb9f66761ee22e0921ee64e7e467c
Iouri Goussev <i.gouss@gmail.com>
2015-05-18 23:10:16 -0400

    Some major reorganization of code:  - network of main loop  - code to getch restaurants from google  - display crash message  - use WebHelper everywhere

--- app/src/main/java/ca/dagar/lunchranker/HeaderFragment.java
+++ app/src/main/java/ca/dagar/lunchranker/HeaderFragment.java
@@ -15,8 +15,7 @@ public class HeaderFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_header, container, false);
     }
